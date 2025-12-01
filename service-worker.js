322bab44e3fde3f497a097422a5c2c15dcb574a7
RawJSON <jayancobky567@gmail.com>
2025-12-01 09:07:40 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'stylish-words-v1-0-cache';
+
+// List all core files to be cached for offline use
+const urlsToCache = [
+    './',
+    'index.html',
+    'script.js',
+    'style.css',
+    'manifest.json',
+    'icon-192.png',
+    'icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); 
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim()); 
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
+            })
+        ))
+    );
+});
